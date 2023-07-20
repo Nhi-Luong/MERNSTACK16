854b95162207382966cb5eb555f0ad6112368f43
Nhi-Luong <124530499+Nhi-Luong@users.noreply.github.com>
2023-07-20 10:13:12 -0700

    update first, and note

--- first.js
+++ first.js
@@ -1,1 +1,14 @@
 console.log("hello world!")
+
+var check_data_type = {
+    name: "Nhi",
+    session: "mernstack"
+}
+
+console.log("check_data_type-" + typeof check_data_type)
+console.log("data is -" + JSON.stringify(check_data_type))
+
+var age = 18
+var isValidId = true 
+
+isValidId && age == 18 ? console.log("valid voter") : console.log("invalid voter")
